0c2f5211c2d6b981960772d010a57f6451a99cd4
Meda Varun Kumar <codedotgo@gmail.com>
2025-01-29 16:13:42 +0530

    Dev1 added m1 method in B class

---
+++ GiTUsignEclipse/src/main/java/com/nt/service/B1.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+public class B1 {
+	public void m1() {
+		System.out.println("M1Method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
