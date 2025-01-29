8f540023b7d1d86a9465e92a4668b21bd1fc4d30
VarunKumarCoder <medavarunkumar@gmail.com>
2025-01-29 17:06:52 +0530

    TL Created Payment branch and added upi payment class

---
+++ GiTUsignEclipse/src/main/java/com/nt/payment/UpiPayment.java
@@ -0,0 +1,8 @@
+package com.nt.payment;
+
+public class UpiPayment {
+	public String doPayment(Long mobileNo, Double amount) {
+		System.out.println("Payment Is Done");
+		return "Payment Done";
+	}
+}
