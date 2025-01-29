acf49548fc3db900fd080fd11a3be35fe239d204
Meda Varun Kumar <codedotgo@gmail.com>
2025-01-29 18:32:50 +0530

    Bug456 is fixed

--- GiTUsignEclipse/src/main/java/com/nt/service/B1.java
+++ GiTUsignEclipse/src/main/java/com/nt/service/B1.java
@@ -4,6 +4,10 @@ public class B1 {
 	public void m1() {
 		System.out.println("M1Method");
 	}
+	
+	public void bug456() {
+		System.out.println("bug456 is fixed");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
